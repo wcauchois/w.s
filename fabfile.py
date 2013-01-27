484fd684a7da50b760c52d9b6633121c883b7956
Bill Cauchois <wcauchois@gmail.com>
2013-01-27 17:43:10 -0500

    convert orig configs to json <3

--- fabfile.py
+++ fabfile.py
@@ -3,15 +3,26 @@ import os, json
 from fabric.api import run, local, settings, abort, env, cd, lcd
 from fabric.operations import put
 
-AWS_BUCKET = open('aws_bucket.txt', 'r').read().strip()
+common_config = json.load(open('config.common.json', 'r'))
+AWS_BUCKET = common_config['AWS_BUCKET']
 AWS_ASSETS_ROOT = 'http://%s.s3.amazonaws.com/' % AWS_BUCKET
+CONFIG_MODES = ('dev', 'prod')
 
-def copy_configs():
-  for target_dir in ('chrome/', 'node/'):
-    for mode in ('dev', 'prod'):
-      local('cp config.%s.js %s' % (mode, target_dir))
+def gen_configs():
+  for mode in CONFIG_MODES:
+    config_json = json.load(open('config.%s.json' % mode, 'r'))
+    config_dict = dict(config_json)
+    config_dict.update(common_config)
+    config_js = """\
+    var %s_config = %s;
+    if (typeof exports != undefined)
+      module.exports = %s_config;
+    """ % (mode, json.dumps(config_dict), mode)
+    for target_dir in ('chrome', 'node'):
+      with open(os.path.join(target_dir, 'config.%s.js' % mode), 'w') as out_stream:
+        out_stream.write(config_js)
 
-def generated_assets():
+def generated_files():
   for dir in ('chrome', 'node'):
     for mode in ('dev', 'prod'):
       yield '%s/config.%s.js' % (dir, mode)
@@ -20,10 +31,10 @@ def generated_assets():
   yield 'chrome/manifest.json'
 
 def clean():
-  for asset in generated_assets():
+  for asset in generated_files():
     local('rm %s' % asset)
 
-def copy_common_assets():
+def copy_common_files():
   local('cp common/post.mustache node/templates/')
   local('cp common/post.mustache chrome/')
 
@@ -38,8 +49,8 @@ def download_asset_scripts():
       local('wget --quiet %s%s' % (AWS_ASSETS_ROOT, script))
 
 def gen():
-  copy_configs()
-  copy_common_assets()
+  gen_configs()
+  copy_common_files()
   chrome_compile()
   download_asset_scripts()
 
