c077fcccb467186e61bc2fa7163a5c49114d8431
Bill Cauchois <wcauchois@gmail.com>
2013-01-27 14:13:36 -0500

    make Makefile to copy configs over

--- config.dev.js
+++ config.dev.js
@@ -1,13 +1,17 @@
+// REMEMBER to `make' after changing this file
+// So it gets copied to chrome/ and node/
 var config = {
-  DB: 'w_s',
-  MONGO_HOST: '127.0.0.1',
-  MONGO_PORT: 27017,
-  HOST: 'local.w.s',
-  PORT: 8080,
-  SOCKET_IO_HOST: 'local.w.s',
-  SOCKET_IO_PORT: 8079,
-  DEBUG: true
+  dev: {
+    DB: 'w_s',
+    MONGO_HOST: '127.0.0.1',
+    MONGO_PORT: 27017,
+    HOST: 'local.w.s',
+    PORT: 8080,
+    SOCKET_IO_HOST: 'local.w.s',
+    SOCKET_IO_PORT: 8079,
+    DEBUG: true
+  }
 };
 if (typeof exports != undefined)
-  exports = config;
+  exports = config.dev;
 
--- config.prod.js
+++ config.prod.js
@@ -1,13 +1,17 @@
+// REMEMBER to `make' after changing this file
+// So it gets copied to chrome/ and node/
 var config = {
-  DB: 'w_s',
-  MONGO_HOST: '127.0.0.1',
-  MONGO_PORT: 27017,
-  HOST: 'w.s',
-  PORT: 8080,
-  SOCKET_IO_HOST: 'w.s',
-  SOCKET_IO_PORT: 8079,
-  DEBUG: false
+  prod: {
+    DB: 'w_s',
+    MONGO_HOST: '127.0.0.1',
+    MONGO_PORT: 27017,
+    HOST: 'w.s',
+    PORT: 8080,
+    SOCKET_IO_HOST: 'w.s',
+    SOCKET_IO_PORT: 8079,
+    DEBUG: false
+  }
 };
 if (typeof exports != undefined)
-  exports = config;
+  exports = config.prod;
 
